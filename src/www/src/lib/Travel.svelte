92e86c2f160d6eebab2da53b4c2be4c0db6ce0f4
Matthew Blott <m4ttb@pm.me>
2022-11-06 17:39:09 +0000

    Data is now displayed from the API source

--- src/www/src/lib/Travel.svelte
+++ src/www/src/lib/Travel.svelte
@@ -1,1 +1,48 @@
-<h2>Travel Component</h2>
+<script>
+
+  import { onMount } from 'svelte'
+  import {data, timeout} from './stores'
+  import {writable} from 'svelte/store'
+
+  const getData = async () => {
+
+    let response = await fetch(`https://api.tfl.gov.uk/Stoppoint/940GZZLUGPS/Arrivals?mode=tube`)
+    let values = await response.json()
+
+    return values
+  }
+
+  onMount(async() => {
+    $data = await getData()
+  })
+
+</script>
+
+<div>
+
+  <table>
+    <thead>
+      <th>Line</th>
+      <th>Destination</th>
+      <th>Platform</th>
+      <th>Direction</th>
+      <th>Expected</th>
+    </thead>
+    <tbody>
+
+      {#each $data as { id, currentLocation, destinationName, platformName, direction, expectedArrival, lineName }}
+
+        <tr>
+          <td>{lineName}</td>
+          <td>{destinationName}</td>
+          <td>{platformName}</td>
+          <td>{direction}</td>
+          <td>{expectedArrival}</td>
+        </tr>
+
+      {/each}
+    </tbody>
+
+  </table>
+
+</div>
